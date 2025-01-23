10d9ce35ab29cf10cb2ba6f608ab91ae3dc4ceeb
24LP5569 24LP5569 <24LP5569@G304-CC0226.local>
2025-01-23 12:54:48 +0800

    carousel

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,16 +1,17 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Search } from "lucide-react";
 import FilmLogo from "./FilmLogo";
+import { Input } from "@/components/ui/input";
 export function Header() {
   return (
     <div className="flex">
       <div className="flex">
         <FilmLogo />
-        <p className="text-[16px] text-[#4338CA] font-bold ">Movie Z</p>
+        <p className="italic text-[16px] text-[#4338CA] font-bold ">Movie Z</p>
       </div>
       <div className="flex w-[355px] rounded-[8px] px-[12px] gap-10 items-center bg-[#71717A]">
-        <Search />{" "}
-        <input className=" outline-none" type="text" placeholder="Search" />
+        <Search />
+        <Input />
       </div>
       <ModeToggle />
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,16 @@ import { MovieCard } from "./_components/MovieCard";
 import response from "./util/response";
 import { Header } from "./_components/Header";
 import { ArrowButton } from "./_components/Button";
+import { NowPlaying } from "./_components/NowPlaying";
 
 export default async function Home() {
   const popular = await response("/movie/popular?language=en-US&page=1");
   const upComing = await response("/movie/upcoming?language=en-US&page=1");
+
   return (
     <div>
       <Header />
+      <NowPlaying />;
       <ArrowButton />
       <div className="grid grid-cols-6 gap-8 px-20 grid-rows-2">
         {upComing.results.map((movie: MovieType, index: number) => {
