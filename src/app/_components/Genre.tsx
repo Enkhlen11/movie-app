522f68fc9909b7f3545992285d370490c64b9079
24LP5569 24LP5569 <24LP5569@G304-CC0226.local>
2025-01-30 12:36:45 +0800

    genre toggle

--- src/app/_components/Genre.tsx
+++ src/app/_components/Genre.tsx
@@ -12,6 +12,11 @@ import { MovieType } from "../util/types";
 import { GenreType } from "../util/genre";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
+import { Toggle } from "@/components/ui/toggle";
+import { ToggleGroup } from "@radix-ui/react-toggle-group";
+import { ToggleGroupItem } from "@/components/ui/toggle-group";
+import { useRouter } from "next/router";
+
 export async function Genre() {
   const genre = await fetch(
     `https://api.themoviedb.org/3/genre/movie/list?language=en`,
@@ -22,36 +27,41 @@ export async function Genre() {
       },
     }
   );
+  const router = useRouter();
+  // const style={color:router.}
   const data = await genre.json();
   // console.log("genre", data);
   const movies = data.genres;
+  const changeHandler = (values: string[]) => {
+    router.push(`/genres?${}`);
   return (
-    <Link
-      href={`/geners?value=${movies.map((movie: GenreType, id: number) => {})}`}
-    >
-      <DropdownMenu>
-        <DropdownMenuTrigger className="flex w-[97px] h-[36px] px-4 py-2 border-[1px] rounded-md text-[14px] gap-2">
-          <ChevronDown className="w-[16px]" />
-          Genre
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className=" w-[577px] h-[333px] ">
-          <DropdownMenuLabel className="text-[24px] font-semibold ">
-            Genres
-          </DropdownMenuLabel>
-          <p className="mb-[4px] text-[16px]">See lists of movies by genre</p>
-          <div className="flex flex-wrap border-t-2 gap-3 pt-[4px]">
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex w-[97px] h-[36px] px-4 py-2 border-[1px] rounded-md text-[14px] gap-2">
+        <ChevronDown className="w-[16px]" />
+        Genre
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className=" w-[577px] h-[333px] ">
+        <DropdownMenuLabel className="text-[24px] font-semibold ">
+          Genres
+        </DropdownMenuLabel>
+        <p className="mb-[4px] text-[16px]">See lists of movies by genre</p>
+        <div className="flex flex-wrap border-t-2 gap-3 pt-[4px]">
+          <ToggleGroup type="single" onValueChange={changeHandler}>
             {movies.map((data: GenreType, index: number) => {
               return (
-                <div className="">
-                  <div className="rounded-[9px] font-semibold text-[12px]  px-[4px] border-[1px] flex justify-center items-center mt-[4px] gap-2">
-                    {data.name} <ChevronRight className="w-[16px]" />
+                <ToggleGroupItem value={data.id.toString()}>
+                  <div key={index}>
+                    <div className="rounded-[9px] font-semibold text-[12px]  px-[4px] border-[1px] flex justify-center items-center mt-[4px] gap-2">
+                      {data.name} <ChevronRight className="w-[16px]" />
+                    </div>
                   </div>
-                </div>
+                </ToggleGroupItem>
               );
             })}
-          </div>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </Link>
+          </ToggleGroup>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
+   
   );
 }
